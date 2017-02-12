0d49121f43ed972b26292e684d0cc4bf61bb1da4
shashi693 <sukenkar@hotmail.com>
2017-02-12 20:16:54 +0530

    Latlong added
    Latlong added

---
+++ app/src/main/java/com/avenueinfotech/yourlocationaddress/MainActivity.java
@@ -0,0 +1,267 @@
+package com.avenueinfotech.yourlocationaddress;
+
+
+import android.app.Dialog;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    GoogleMap mGoogleMap;
+    GoogleApiClient mGoogleApiClient;
+    private TextView textView;
+    ConnectionDetector cd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-1183672799205641~7719191016");
+
+        if (googleServicesAvailable()) {
+            Toast.makeText(this, "GPS Enabled", Toast.LENGTH_SHORT).show();
+            setContentView(R.layout.activity_main);
+            initMap();
+        } else {
+            Toast.makeText(this, "Enable GPS", Toast.LENGTH_LONG).show();
+        }
+
+//        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
+//        mapFragment.getMapAsync(this);
+
+        cd = new ConnectionDetector(this);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+        if (cd.isConnected()) {
+            Toast.makeText(MainActivity.this, "Internet Connected", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(MainActivity.this, "Please Enable Internet & GPS for your Location", Toast.LENGTH_LONG).show();
+        }
+
+        // Load an ad into the AdMob banner view.
+        AdView adView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .setRequestAgent("android_studio:ad_template").build();
+        adView.loadAd(adRequest);
+
+        textView = (TextView) findViewById(R.id.textView);
+//
+    }
+
+    public boolean googleServicesAvailable() {
+        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+        int isAvailable = api.isGooglePlayServicesAvailable(this);
+        if (isAvailable == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (api.isUserResolvableError(isAvailable)) {
+            Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "Cant connect to play services", Toast.LENGTH_LONG).show();
+        }
+        return false;
+    }
+
+    private void initMap() {
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+//        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        mGoogleMap.setMyLocationEnabled(true);
+//        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+//
+//        mGoogleMap.addMarker(new MarkerOptions()
+//                .title("Sydney")
+//                .snippet("The most populous city in Australia.")
+//                .position(sydney));
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        mGoogleApiClient.connect();
+
+
+
+    }
+
+
+//        if(googleSevicesAvailable()){
+//            Toast.makeText(this, "play services exits", Toast.LENGTH_SHORT).show();
+//            setContentView(R.layout.activity_main);
+//            initMap();
+//        } else {
+//
+//        }
+//    }
+//
+//    private void initMap() {
+//        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.mapFragment);
+//        mapFragment.getMapAsync(this);
+//    }
+//
+//    public boolean googleSevicesAvailable(){
+//        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+//        int isAvailable = api.isGooglePlayServicesAvailable(this);
+//        if(isAvailable == ConnectionResult.SUCCESS){
+//            return true;
+//        } else  if (api.isUserResolvableError(isAvailable)){
+//            Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
+//            dialog.show();
+//        } else {
+//            Toast.makeText(this, "canot concet to play sevrvices", Toast.LENGTH_SHORT).show();
+//        }
+//        return false;
+//    }
+//
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//        mGoogleMap = googleMap;
+//    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mapTypeNone:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+                break;
+            case R.id.mapTypeNormal:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                break;
+            case R.id.mapTypeSatellite:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                break;
+            case R.id.mapTypeTerrain:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                break;
+            case R.id.mapTypeHybrid:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                break;
+
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    LocationRequest mLocationRequest;
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(30000);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        Log.i("LOG", "onConnection(" + bundle + ")");
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+//        Location l = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+//        if(l != null){
+//            Log.i("LOG", "Lat: "+l.getLatitude());
+//            Log.i("LOG", "Log: "+l.getLongitude());
+//            tvCoordiante.setText(l.getLatitude()+" | "+l.getLongitude());
+//
+//        }
+//
+//        startLocationUpdate();
+
+
+    }
+//    private void initLocationRequest() {
+//        mLocationRequest = new LocationRequest();
+//        mLocationRequest.setInterval(5000);
+//        mLocationRequest.setFastestInterval(2000);
+//        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+//    }
+
+//    private void startLocationUpdate() {
+//        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            return;
+//        }
+//        initLocationRequest();
+//        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, MainActivity.this);
+//    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(location == null){
+            Toast.makeText(this, "Cant get current location", Toast.LENGTH_LONG).show();
+        } else {
+            LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, 19);
+            mGoogleMap.animateCamera(update);
+            textView.setText("Long: " + location.getLongitude() + " Lat: " + location.getLatitude() + " Accurcy:" + location.getAccuracy() +"mts" + " Altitude: " + location.getAltitude() );
+
+        }
+
+//        tvCoordiante.setText(Html.fromHtml("Location: "+location.getLatitude()+"<br />"+
+//                "Longitude: "+location.getLongitude()+"<br />'"+
+//                "Bearing: "+location.getBearing()+"<br />"+
+//                "Altitude: "+location.getAltitude()+"<br />"+
+//                "Speed: "+location.getSpeed()+"<br />"+
+//                "Provider: "+location.getProvider()+"<br />"+
+//                "Accurucy: "+location.getAccuracy()+"<br />"+
+//                "Speed: "+ DateFormat.getTimeFormat(this).format(new Date())+"<br +>"
+//        ));
+    }
+}
